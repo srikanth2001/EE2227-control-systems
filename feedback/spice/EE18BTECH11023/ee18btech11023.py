d877c41f79a933306e198611ab2f0aaa09362f4f
srikanth2001 <47571970+srikanth2001@users.noreply.github.com>
2020-05-17 01:54:14 -0400

    Update ee18btech11023.py

--- feedback/spice/EE18BTECH11023/ee18btech11023.py
+++ feedback/spice/EE18BTECH11023/ee18btech11023.py
@@ -13,15 +13,16 @@ data=np.loadtxt('./spice/ee18btech11023/ee18btech11023.dat')
 plt.plot(data[:,0],data[:,1])
 plt.grid()
 plt.xlabel("time")
-plt.ylabel("Step response")
+plt.ylabel("Amplitude")
 plt.title('Spice simulation for feedback amplifier')
 #plt.show()
 #if using termux
 
 
 
-plt.savefig('./figs/ee18btech11023/ee18btech11023_output.pdf')
+plt.savefig('./figs/ee18btech11023/ee18btech11023_output.png')
 plt.savefig('./figs/ee18btech11023/ee18btech11023_output.eps')
 subprocess.run(shlex.split("termux-open ./figs/ee18btech11023/ee18btech11023_output.pdf"))
 #else
 #plt.show()
+
